bb46250492afb597ce34e15523102cb2520865a6
Juan De Leon <dele17822@uvg.edu.gt>
2018-08-19 19:03:01 -0600

    Clases

--- src/sample/items/Item.java
+++ src/sample/items/Item.java
@@ -1,7 +1,47 @@
 package sample.items;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Item {
 
+    private SimpleStringProperty itemName;
+    private SimpleIntegerProperty itemQuant;
+    private SimpleDoubleProperty itemPrice;
+    private Boolean isPending = true;
 
+    public Item(String itemName, Integer itemQuant, Double itemPrice){
+        this.itemName = new SimpleStringProperty();
+        this.itemQuant = new SimpleIntegerProperty();
+        this.itemPrice = new SimpleDoubleProperty();
 
+        this.setItemName(itemName);
+        this.setItemQuant(itemQuant);
+        this.setItemPrice(itemPrice);
+    }
+
+    private void setItemName(String itemName){
+        this.itemName.set(itemName);
+    }
+
+    private void setItemQuant(Integer itemQuant){
+        this.itemQuant.set(itemQuant);
+    }
+
+    private void setItemPrice(Double itemPrice){
+        this.itemPrice.set(itemPrice);
+    }
+
+    public void setIsPending(){
+        this.isPending = !isPending;
+    }
+
+    public boolean getIsPending(){
+        return this.isPending;
+    }
+
+    public SimpleDoubleProperty getItemPrice(){
+        return this.itemPrice;
+    }
 }
--- src/sample/lists/List.java
+++ src/sample/lists/List.java
@@ -8,11 +8,50 @@ import sample.items.Item;
 
 public class List {
 
-    SimpleStringProperty listName;
-    SimpleIntegerProperty pendingItems;
-    SimpleDoubleProperty pendingEstimate;
-    ObservableList<Item> listItems;
+    //Atributos
+    private SimpleStringProperty listName;
+    private SimpleStringProperty listDescription;
+    private ObservableList<Item> itemsList;
+    private SimpleIntegerProperty pendingItems;
+    private SimpleDoubleProperty pendingEstimate;
 
-    
+    //Constructor
+    public List(String listName, String listDescription){
 
+        this.listName = new SimpleStringProperty();
+        this.listDescription = new SimpleStringProperty();
+
+        this.setListName(listName);
+        this.setListDescription(listDescription);
+    }
+
+    private void setListName(String listName){
+        this.listName.set(listName);
+    }
+
+    private void setListDescription(String listDescription){
+        this.listDescription.set(listDescription);
+    }
+
+    public void addItem(Item item){
+        this.itemsList.add(item);
+        this.setPendingItems();
+        this.setPendingEstimate();
+    }
+
+    public void setPendingItems(){//Cuenta cuantos articulos hay pendientes
+        int ctr = 0;
+        for (Item item: itemsList) {
+            if (item.getIsPending()){
+                ctr++;
+            }
+        }
+        this.pendingItems.set(ctr);
+    }
+
+    public void setPendingEstimate(){
+        for (Item item: itemsList) {
+
+        }
+    }
 }
